5445d8e134c22b581e48218c9237d3cf12272f81
Achelia <achelia.selim@smail.th-koeln.de>
2021-09-03 22:50:47 +0200

    update enki-website

--- server/server.js
+++ server/server.js
@@ -33,6 +33,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://enki-cart.herokuapp.com');
     res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
     res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5501');
+    res.header('Access-Control-Allow-Origin', 'https://enki-store.herokuapp.com');
     //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Credentials, Cookie, Set-Cookie');
     res.header('Access-Control-Allow-Credentials', 'true');
