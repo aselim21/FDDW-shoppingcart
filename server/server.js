5f2525ca1ddb2ff96d2764059187c69b83ff2f2b
Achelia <achelia.selim@smail.th-koeln.de>
2021-09-03 23:26:52 +0200

    update

--- server/server.js
+++ server/server.js
@@ -29,12 +29,16 @@ app.set('trust proxy', 1) // trust first proxy
 app.use(express.json());
 app.use(cookieParser());
 app.use((req, res, next) => {
-    //https://enki-cart.herokuapp.com ------ http://127.0.0.1:5500 ---
-    res.header('Access-Control-Allow-Origin', 'https://enki-cart.herokuapp.com');
-    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
-    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5501');
-    res.header('Access-Control-Allow-Origin', 'https://enki-store.herokuapp.com');
-    //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
+    const corsWhitelist = [
+        'https://enki-cart.herokuapp.com',
+        'http://127.0.0.1:5500',
+        'http://127.0.0.1:5501',
+        'http://127.0.0.1:3000',
+        'https://enki-store.herokuapp.com'
+    ];
+    if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+    }
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Credentials, Cookie, Set-Cookie');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DEL, OPTION, HEAD');
