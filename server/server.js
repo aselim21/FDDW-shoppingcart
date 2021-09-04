b61b9c671f2ee66ac81c9e574078455d9ae8ece4
Achelia <achelia.selim@smail.th-koeln.de>
2021-09-04 16:05:25 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -155,9 +155,10 @@ app.get('/products/:id', (req, res) => {
     console.log(book_id);
     db_connectAndDo(db_findBookId, false, db_collection_products, db_enki_products, book_id)
         .then((result) => {
-             const data = {"book_details": result}
-            //res.json({book_details : result})
-            res.send(data);
+            //  const data = {"book_details": result}
+            // //res.json({book_details : result})
+            // console.log(data);
+            res.send(result);
             // console.log(data);
         });
 
