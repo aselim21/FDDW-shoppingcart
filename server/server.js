7733ba0670724dbe28bf2f23937effd5f54acdc9
Achelia <achelia.selim@smail.th-koeln.de>
2021-09-10 17:22:34 +0200

    without http headers

--- server/server.js
+++ server/server.js
@@ -39,25 +39,25 @@ var corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
-app.use((req, res, next) => {
-    // const corsWhitelist = [
-    //     'http://127.0.0.1:5500',
-    //     'http://127.0.0.1:5501',
-    //     'http://127.0.0.1:3000',
-    //     'http://127.0.0.1:3001',
-    //     'https://enki-bookstore.herokuapp.com',
-    //     'https://enki-product.herokuapp.com'
-    // ];
-    // if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
-    //     res.header('Access-Control-Allow-Origin', req.headers.origin);
-    // }
-    // res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Credentials, Cookie, Set-Cookie');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTION, HEAD');
-    //res.header('Access-Control-Request-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Credentials ');
-    next();
-});
+// app.use((req, res, next) => {
+//     // const corsWhitelist = [
+//     //     'http://127.0.0.1:5500',
+//     //     'http://127.0.0.1:5501',
+//     //     'http://127.0.0.1:3000',
+//     //     'http://127.0.0.1:3001',
+//     //     'https://enki-bookstore.herokuapp.com',
+//     //     'https://enki-product.herokuapp.com'
+//     // ];
+//     // if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
+//     //     res.header('Access-Control-Allow-Origin', req.headers.origin);
+//     // }
+//     // res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Credentials, Cookie, Set-Cookie');
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTION, HEAD');
+//     //res.header('Access-Control-Request-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Credentials ');
+//     next();
+// });
 
 mongoose.connect(MongodbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => app.listen(PORT, () => console.log(`Listening on port ${PORT}...`)))
