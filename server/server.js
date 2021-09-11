29e54ddfd241924be56ececd85a6148da50c9940
Achelia <achelia.selim@smail.th-koeln.de>
2021-09-11 12:10:52 +0200

    preflightContinue: true,

--- server/server.js
+++ server/server.js
@@ -17,7 +17,9 @@ const app = express();
 
 var corsOptions = {
     origin: ['https://enki-bookstore.herokuapp.com'],
-    credentials: true
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    credentials: true,
+    preflightContinue: true
 }
 
 app.use(cors(corsOptions));
